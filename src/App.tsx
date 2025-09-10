c399ec61fb1e5b2f08f20f0034ce8467b951c7ee
Uzini <43294422+Uziniii@users.noreply.github.com>
2025-09-10 11:57:04 +0200

    add: search filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,40 @@
+import { useMemo, useState } from "react"
 import { Mail } from "./components/mail"
+import { Input } from "./components/ui/input"
 import { useMails } from "./hooks/useMails"
 
 function App() {
-  const { isLoading, resume, mails } = useMails()
+  const [search, setSearch] = useState("")
+  const { isLoading, error, resume, mails } = useMails()
+
+  const filteredMails = useMemo(() => {
+    if (!search) return mails
+    return mails.filter(mail =>
+      mail.subject.toLowerCase().includes(search.toLowerCase()) ||
+      mail.from.toLowerCase().includes(search.toLowerCase())
+    )
+  }, [mails, search])
 
   if (isLoading) return <div>Loading...</div>
+  if (error) return <div>
+    <p>Make sure that your emails-today.json is placed in the public directory</p>
+    <p >
+      Error: {error.message}
+    </p>
+  </div>
 
-  return <div className="mx-auto p-4">
-    <div className="border rounded mb-4 p-4">
+  return <div className="mx-auto flex flex-col">
+    <div className="border rounded p-4 m-4 mb-0">
       <h2 className="text-2xl font-bold mb-4">Résumé du jour</h2>
       {resume}
     </div>
-    <div className="border rounded p-4 mb-4">
-      {mails.map(mail => <Mail key={mail.id} mail={mail} />)}
+    <header className="sticky top-0 p-4 bg-background/50 backdrop-blur-xl">
+      <Input className="rounded" placeholder="Search in mails" onChange={(e) => setSearch(e.target.value)} value={search} />
+    </header>
+    <div className="border rounded p-4 m-4 mt-0">
+      {filteredMails.map((mail, i, array) =>
+        <Mail key={mail.id} mail={mail} isLast={i === array.length - 1} />
+      )}
     </div>
   </div>
 }
--- src/components/mail.tsx
+++ src/components/mail.tsx
@@ -1,21 +1,49 @@
-import type { Mail } from "@/hooks/useMails"
+import type { Mail as MailType } from "@/hooks/useMails"
 import { Badge } from "./ui/badge"
+import React, { useMemo } from "react"
+import { Separator } from "./ui/separator"
 
 type MailProps = {
-  mail: Mail
+  mail: MailType
+  isLast: boolean
 }
 
-export function Mail({ mail }: MailProps) {
-  const time = new Intl.DateTimeFormat('fr-FR', {
-    hour: '2-digit',
-    minute: '2-digit',
-  }).format(new Date(mail.date));
+export const Mail = React.memo(function Mail({ mail, isLast }: MailProps) {
+  const [date, time, isToday] = useMemo(() => {
+    const now = new Date();
+    const date = new Date(mail.date);
+
+    const isToday =
+      date.getDate() === now.getDate() &&
+      date.getMonth() === now.getMonth() &&
+      date.getFullYear() === now.getFullYear();
+
+    const time = new Intl.DateTimeFormat('fr-FR', {
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: false,
+    }).format(date);
+
+    const todayDate = new Intl.DateTimeFormat('fr-FR', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+    }).format(date);
+
+    return [todayDate, time, isToday];
+  }, [mail])
 
   return <div className="rounded mb-4">
-    <div className="flex justify-between items-start mb-0.5">
-      <h2 className="text-xl font-bold mb-1">{mail.subject}</h2>
-      <Badge>{time}</Badge>
+    <div className="flex justify-between items-start mb-0.5 gap-2">
+      <h2 className="text-xl font-bold mb-1 flex">{mail.subject}</h2>
+      <div className="flex flex-col gap-1">
+        {isToday ? <Badge>{time}</Badge> : <>
+          <Badge>{date}</Badge>
+          <Badge>{time}</Badge>
+        </>}
+      </div>
     </div>
     <p className="text-gray-600 break-words">From: {mail.from}</p>
+    {!isLast && <Separator className="mt-4" />}
   </div>
-}
+})
